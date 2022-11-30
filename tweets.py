de782cc8e39e20869c668aa876a6bec66b8e56fb
nabilfikrisp <mnabilfikrisp@gmail.com>
2022-11-30 21:15:39 +0700

    second commit

--- tweets.py
+++ tweets.py
@@ -4,16 +4,19 @@ import pandas as pd
 query = "'world cup' lang:en until:2022-11-30 since:2022-11-01"
 tweets = []
 limit = 2000
+prevTweet = ''
 
 for tweet in sntwitter.TwitterSearchScraper(query).get_items():
   if len(tweets) == limit:
     break
   else:
-    tweets.append([
-      tweet.date,
-      tweet.user.username,
-      tweet.content
-    ])
+    if prevTweet != tweet.content:
+      tweets.append([
+        tweet.date,
+        tweet.user.username,
+        tweet.content
+      ])
+  prevTweet = tweet.content
 
 df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
 df.to_csv('tweets.csv')
